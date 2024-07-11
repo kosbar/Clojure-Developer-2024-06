2156d27c50642f53f565dd4e5b5608f8e5d99f9f
kosbar <kosbar28@gmail.com>
2024-07-11 15:40:25 +0500

    Homework for lesson Otus-02

--- otus-02/src/otus_02/homework/palindrome.clj
+++ otus-02/src/otus_02/homework/palindrome.clj
@@ -1,6 +1,11 @@
 (ns otus-02.homework.palindrome
   (:require [clojure.string :as string]))
 
-
-(defn is-palindrome [test-string])
-
+(defn is-palindrome
+  "Func for check strings on palyndrome"
+  [s]
+  {:pre [(not= s nil)]}
+  (let [test-string (as-> s $
+                        (string/lower-case $)
+                        (string/replace $ #"[^a-z]" ""))]
+    (= test-string (str/reverse test-string))))
--- otus-02/src/otus_02/homework/pangram.clj
+++ otus-02/src/otus_02/homework/pangram.clj
@@ -1,6 +1,11 @@
 (ns otus-02.homework.pangram
   (:require [clojure.string :as string]))
 
-
-(defn is-pangram [test-string])
-
+(defn is-pangram [test-string]
+  "Func for check strings on pangram"
+  {:pre [(not= test-string nil)]}
+  (let [set-chars (-> test-string
+                       string/lower-case
+                       (string/replace #"[^a-z]" "")
+                       set)]
+    (= (count set-chars) 26)))
